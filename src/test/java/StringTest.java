24a406e030e85d028387db93992346a9999b6382
huiquan1 <huiquan1@163.com>
2015-06-30 23:52:47 +0800

    StringTest

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,27 +4,30 @@ import static org.hamcrest.Matchers.*;
 
 public class StringTest{
 
+	@Test
 	public void test(){
 		String a ="what are you nong sha lei";
 		String b ="what are you nong sha lei";
 		assertThat(a,is(b));
 		assertThat(a,equalTo(b));
 	}
+	@Test
 	public void test2(){
 		String a ="what are you nong sha lei";
 		String b =new String("what are you nong sha lei");
-		assertThat(a,not(b));
+		assertTrue(a!= b);
 		assertThat(a,equalTo(b));
 	}
 
+	@Test
 	public void test3(){
 		String a ="what are you nong sha lei";
-		assertThat(a,not(getStr()));
+		assertTrue(a== getStr());
 		assertThat(a,equalTo(getStr()));
 	}
 	private String getStr(){
-		String b ="what are you nong sha lei";
-		return b;
+		String b ="what are you " + "nong sha lei";
+		return b ;
 	}
 
 }
